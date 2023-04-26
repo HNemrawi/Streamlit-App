de41d419d9cb3218103749d5f4fe52034bc92c80
HNemrawi <hnimrawi@gmail.com>
2023-04-26 14:06:45 -0500

    Second commit

--- looker_utils.py
+++ looker_utils.py
@@ -5,10 +5,11 @@ import streamlit as st
 # Load the Looker SDK configuration from st.secrets
 
 base_url = "https://looker.clarityhs.com:19999"
-client_id = st.secrets["client_id"]
-client_secret = st.secrets["client_secret"]
+client_id = st.secrets["Looker"]["client_id"]
+client_secret = st.secrets["Looker"]["client_secret"]
 verify_ssl = True
 
 # Initialize the Looker SDK with the configuration
 sdk = looker_sdk.init40()
+
 
